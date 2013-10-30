3f2421a68f482f7fa60e4990151454e4c0b33ddf
aogilvie <aogilvie@wizcorp.jp>
2013-10-30 10:38:40 +0900

    changed messenger script injection to evaluate string rather than load from file

--- ios/project/Plugins/WizViewManagerPlugin/WizWebView.m
+++ ios/project/Plugins/WizViewManagerPlugin/WizWebView.m
@@ -158,9 +158,69 @@ static CDVPlugin* viewManager;
     }
 
     // Load in wizViewMessenger
-    NSString *resourcePath = [NSString stringWithFormat:@"%@/www/%@", [[NSBundle mainBundle] resourcePath], @"phonegap/plugin/wizViewMessenger/wizViewMessenger.js" ];
-    NSString *script = [NSString stringWithContentsOfFile:resourcePath encoding:NSUTF8StringEncoding error:NULL];
-    [theWebView stringByEvaluatingJavaScriptFromString:script];
+    NSString *js = @"var WizViewMessenger = function () {}; \
+    WizViewMessenger.prototype.postMessage = function (message, targetView) { \
+        var type; \
+        if (Object.prototype.toString.call(message) === '[object Array]') { \
+            type = 'Array'; \
+            message = JSON.stringify(message); \
+        } else if (Object.prototype.toString.call(message) === '[object String]') { \
+            type = 'String'; \
+        } else if (Object.prototype.toString.call(message) === '[object Number]') { \
+            type = 'Number'; \
+            message = JSON.stringify(message); \
+        } else if (Object.prototype.toString.call(message) === '[object Boolean]') { \
+            type = 'Boolean'; \
+            message = message.toString(); \
+        } else if (Object.prototype.toString.call(message) === '[object Function]') { \
+            type = 'Function'; \
+            message = message.toString(); \
+        } else if (Object.prototype.toString.call(message) === '[object Object]') { \
+            type = 'Object'; \
+            message = JSON.stringify(message); \
+        } else { \
+            console.error('WizViewMessenger posted unknown type!'); \
+            return; \
+        } \
+ \
+        var iframe = document.createElement('IFRAME'); \
+        iframe.setAttribute('src', 'wizPostMessage://'+ window.encodeURIComponent(window.name) + '?' + window.encodeURIComponent(targetView) + '?' + window.encodeURIComponent(message) + '?' + type ); \
+        document.documentElement.appendChild(iframe); \
+        iframe.parentNode.removeChild(iframe); \
+        iframe = null; \
+    }; \
+ \
+    WizViewMessenger.prototype.__triggerMessageEvent = function (origin, target, data, type) { \
+        if (type === 'Array') { \
+            data = JSON.parse(data); \
+        } else if (type === 'String') { \
+            /* Stringy String String */ \
+        } else if (type === 'Number') { \
+            data = JSON.parse(data); \
+        } else if (type === 'Boolean') { \
+            data = Boolean(data); \
+        } else if (type === 'Function') { \
+            /* W3C says nothing about functions, will be returned as string. */ \
+        } else if (type === 'Object') { \
+            data = JSON.parse(data); \
+        } else { \
+            console.error('Message Event received unknown type!'); \
+            return; \
+        } \
+ \
+        var event = document.createEvent('HTMLEvents'); \
+        event.initEvent('message', true, true); \
+        event.eventName = 'message'; \
+        event.memo = { }; \
+        event.origin = origin; \
+        event.source = target; \
+        event.data = data; \
+        dispatchEvent(event); \
+    }; \
+ \
+    window.wizViewMessenger = new WizViewMessenger(); ";
+    
+    [theWebView stringByEvaluatingJavaScriptFromString:js];
 }
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSMutableURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
